04b523b8d0ee071b93eb8413031c00390f991a6c
Fišer, Václav <fiserva3@fjfi.cvut.cz>
2021-04-25 22:18:39 +0200

    fixed movement vector calculation

--- spaceship.c
+++ spaceship.c
@@ -1,5 +1,14 @@
 #include "spaceship.h"
 
+// move someplace else
+#define M_PI   3.14159265358979323846264338327950288
+double deg2rad(double degrees){
+    return degrees * M_PI / 180.0;
+}
+double rad2deg(double radians){
+    return radians * 180.0 / M_PI;
+}
+
 void spaceshipInit(Spaceship* sp){
     sp->movementVec[0] = 1.0;
     sp->movementVec[1] = 0.0;
@@ -11,8 +20,9 @@ void spaceshipInit(Spaceship* sp){
     sp->invincible = false;
 }
 
+// check if calc is correct
 void spaceshipUpdate(Spaceship *sp){
-    sp->movementVec[0] += sin(sp->headingAngle) * sp->engineThrust;
-    sp->movementVec[1] += cos(sp->headingAngle) * sp->engineThrust;
+    sp->movementVec[0] += cos(deg2rad(sp->headingAngle)) * sp->engineThrust;
+    sp->movementVec[1] += sin(deg2rad(sp->headingAngle)) * sp->engineThrust;
 
 }
